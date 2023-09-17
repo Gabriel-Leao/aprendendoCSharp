6754e119b4e681427bce6b43a4c1e229f88fba0d
Gabriel-Leao <gabriel.leao@catskillet.com>
2023-09-16 23:22:46 -0300

    feat: adicinando bandas a lista e exibindo essas bandas

--- PrimeiroProjeto/Program.cs
+++ PrimeiroProjeto/Program.cs
@@ -1,12 +1,20 @@
 ﻿using static System.Console;
 
-void ShowMessage(string message)
-{
-    WriteLine(message);
-}
+var bandsList = new List<string> {"Beatles", "Led Zeppelin"};
+
+ShowMenuOptions();
+return;
 
 void ShowMenuOptions()
 {
+    ShowLogo(@"
+    ░██████╗░█████╗░██████╗░███████╗███████╗███╗░░██╗  ░██████╗░█████╗░██╗░░░██╗███╗░░██╗██████╗░
+    ██╔════╝██╔══██╗██╔══██╗██╔════╝██╔════╝████╗░██║  ██╔════╝██╔══██╗██║░░░██║████╗░██║██╔══██╗
+    ╚█████╗░██║░░╚═╝██████╔╝█████╗░░█████╗░░██╔██╗██║  ╚█████╗░██║░░██║██║░░░██║██╔██╗██║██║░░██║
+    ░╚═══██╗██║░░██╗██╔══██╗██╔══╝░░██╔══╝░░██║╚████║  ░╚═══██╗██║░░██║██║░░░██║██║╚████║██║░░██║
+    ██████╔╝╚█████╔╝██║░░██║███████╗███████╗██║░╚███║  ██████╔╝╚█████╔╝╚██████╔╝██║░╚███║██████╔╝
+    ╚═════╝░░╚════╝░╚═╝░░╚═╝╚══════╝╚══════╝╚═╝░░╚══╝  ╚═════╝░░╚════╝░░╚═════╝░╚═╝░░╚══╝╚═════╝░
+    ");
     WriteLine("\n[1] para registrar uma banda");
     WriteLine("[2] para mostrar todas as bandas");
     WriteLine("[3] para avaliar uma banda");
@@ -22,10 +30,10 @@ void ShowMenuOptions()
             WriteLine("\nAté mais :)");
             break;
         case 1:
-            WriteLine($"\nVocê digitou a opção: {option}");
+            RegisterBand();
             break;
         case 2:
-            WriteLine($"\nVocê digitou a opção: {option}");
+            ListBands();
             break;
         case 3:
             WriteLine($"\nVocê digitou a opção: {option}");
@@ -39,13 +47,40 @@ void ShowMenuOptions()
     }
 }
 
-ShowMessage(@"
-    ░██████╗░█████╗░██████╗░███████╗███████╗███╗░░██╗  ░██████╗░█████╗░██╗░░░██╗███╗░░██╗██████╗░
-    ██╔════╝██╔══██╗██╔══██╗██╔════╝██╔════╝████╗░██║  ██╔════╝██╔══██╗██║░░░██║████╗░██║██╔══██╗
-    ╚█████╗░██║░░╚═╝██████╔╝█████╗░░█████╗░░██╔██╗██║  ╚█████╗░██║░░██║██║░░░██║██╔██╗██║██║░░██║
-    ░╚═══██╗██║░░██╗██╔══██╗██╔══╝░░██╔══╝░░██║╚████║  ░╚═══██╗██║░░██║██║░░░██║██║╚████║██║░░██║
-    ██████╔╝╚█████╔╝██║░░██║███████╗███████╗██║░╚███║  ██████╔╝╚█████╔╝╚██████╔╝██║░╚███║██████╔╝
-    ╚═════╝░░╚════╝░╚═╝░░╚═╝╚══════╝╚══════╝╚═╝░░╚══╝  ╚═════╝░░╚════╝░░╚═════╝░╚═╝░░╚══╝╚═════╝░
-");
+void ShowLogo(string message)
+{
+    WriteLine(message);
+}
 
-ShowMenuOptions();
+void RegisterBand()
+{
+    Clear();
+    WriteLine("Rigistro de banda");
+    Write("Digite o nome da banda: ");
+    var bandName = ReadLine()!;
+    bandsList.Add(bandName);
+    WriteLine($"\nA banda {bandName}, foi registrada com sucesso!");
+    Thread.Sleep(2000);
+    Clear();
+    ShowMenuOptions();
+}
+
+void ListBands()
+{
+    Clear();
+    WriteLine("===================");
+    WriteLine("Bandas registradas");
+    WriteLine("===================\n");
+    for (var i = 0; i < bandsList.Count; i++)
+    {
+        WriteLine($"{i + 1} - {bandsList[i]}");
+    }
+    // foreach (var band in bandsList)
+    // {
+    //     WriteLine(band);
+    // }
+    WriteLine("\nDigite qualquer coisa para voltar a tela inicial");
+    ReadKey();
+    Clear();
+    ShowMenuOptions();
+}
